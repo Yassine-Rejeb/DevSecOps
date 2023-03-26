cb8f85ecfb8a304ad28710c84a949ba3e9b8ad89
ansible <ansible@node1.rhce.class.com>
2023-03-26 06:19:55 +0100

    add a broken node creation feature

--- provisionning_pipeline.sh
+++ provisionning_pipeline.sh
@@ -4,4 +4,6 @@ rm -rf ~/.ssh/known_hosts
 terraform -chdir=terraform apply -auto-approve
 sudo sed -i "3s/.*/$(terraform -chdir=./terraform output -raw jenkins_public_ip)	remote_jenkins/" /etc/hosts
 sudo sed -i "4s/.*/$(terraform -chdir=./terraform output -raw prod_public_ip)	production_server/" /etc/hosts
-ansible-playbook --private-key /home/ansible/.ssh/jenkins jenkins.yml -i inventory
+ansible-playbook --private-key /home/ansible/.ssh/jenkins jenkins.yml -i inventory 
+ansible-playbook --private-key /home/ansible/.ssh/prod jenkins2.yml
+ansible-playbook --private-key /home/ansible/.ssh/jenkins jenkins3.yml
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -118,3 +118,9 @@ output "jenkins_public_ip"{
 output "prod_public_ip" {
 	value = aws_instance.production.public_ip
 }
+output "prod_private_ip" {
+        value = aws_instance.production.private_ip
+}
+output "jenkins_private_ip"{
+        value = aws_instance.jenkins.private_ip
+}
