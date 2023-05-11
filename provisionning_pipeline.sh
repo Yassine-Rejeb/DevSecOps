ca461666f175f46a58f68d927944c72047c7ce41
root <root@controle.example.com>
2023-05-11 08:00:02 -0700

    test

--- provisionning_pipeline.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-rm -rf ~/.ssh/known_hosts
-terraform -chdir=terraform apply -auto-approve
-sudo sed -i "3s/.*/$(terraform -chdir=./terraform output -raw jenkins_public_ip)	remote_jenkins/" /etc/hosts
-sudo sed -i "4s/.*/$(terraform -chdir=./terraform output -raw prod_public_ip)	production_server/" /etc/hosts
-ansible-playbook --private-key /home/ansible/.ssh/jenkins jenkins.yml 
-ansible-playbook --private-key /home/ansible/.ssh/prod jenkins2.yml
-#ansible-playbook --private-key /home/ansible/.ssh/jenkins jenkins3.yml
